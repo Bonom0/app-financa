afe1fef8f2f715123968598edbcc059b304977da
caio <caioazbonomo@gmail.com>
2025-03-11 21:59:47 -0300

    Adicionado useState para nome, email e password

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Platform } from 'react-native'
 import { 
     Background, 
@@ -12,9 +12,12 @@ import { AuthContext } from "../../contexts/auth";
 
 export default function SignIn(){
     const { signUp } = useContext(AuthContext)
-    
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     function handleSignUp(){
-        signUp();
+        signUp(nome, email, password);
     }
 
     return(
@@ -27,18 +30,25 @@ export default function SignIn(){
                 <AreaInput>
                     <Input
                         placeholder="Nome"
+                        value={nome}
+                        onChangeText={ (text) => setNome(text) }
                     />
                 </AreaInput>
 
                 <AreaInput>
                     <Input
                         placeholder="Seu E-mail"
+                        value={email}
+                        onChangeText={ (text) => setEmail(text) }
                     />
                 </AreaInput>
 
                 <AreaInput>
                     <Input
                         placeholder="Sua senha"
+                        value={password}
+                        onChangeText={ (text) => setPassword(text) }
+                        secureTextEntry={true}
                     />
                 </AreaInput>
 
