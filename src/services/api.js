dc5957d44b40ebf08f09f8996f49b308710e0b71
Bonom0 <caioazbonomo@gmail.com>
2025-03-12 00:34:58 -0300

    adicionado o npx expo

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://172.16.0.7:3333'
+    // baseURL: 'http://172.16.0.7:3333' //pc fag
+    baseURL: 'http://192.168.1.120:3333' //pc casa
 })
 
 export default api;
