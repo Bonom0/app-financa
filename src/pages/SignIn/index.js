3b88cd22c4a6d70a64c7140863696ca747a8424a
caio <caioazbonomo@gmail.com>
2025-03-13 19:54:42 -0300

    Importado função de signIn para a função handleLogin

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { Platform } from 'react-native'
 import { 
     Background, 
@@ -12,10 +12,18 @@ import {
     LinkText
 } from "./styles";
 import { useNavigation } from "@react-navigation/native";
+import { AuthContext } from "../../contexts/auth";
 
 export default function SignIn(){
     const navigation = useNavigation();
-    
+    const { signIn, LoadingAuth} = useContext(AuthContext)
+    const {email, setEmail} = useState('');
+    const {password, setPassword} = useState('');
+
+    function handleLogin(){
+        signIn();
+    }
+
     return(
         <Background>
             <Container
@@ -29,16 +37,20 @@ export default function SignIn(){
                 <AreaInput>
                     <Input
                         placeholder="Seu email"
+                        value={email}
+                        onChangeText={ (text) => setEmail(text)}
                     />
                 </AreaInput>
 
                 <AreaInput>
                     <Input
                         placeholder="Sua senha"
+                        value={password}
+                        onChangeText={ (text) => setPassword(text)}
                     />
                 </AreaInput>
 
-                <SubmitButton activeOpacity={0.8}>
+                <SubmitButton activeOpacity={0.8} onPress={handleLogin}>
                     <SubmitText>Acessar</SubmitText>
                 </SubmitButton>
 
