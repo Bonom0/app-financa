85ce4b5cad304c7842f13e226f1c1c6f4ec22010
caio <caioazbonomo@gmail.com>
2025-03-13 20:17:42 -0300

    Alterado para o ip de casa

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: 'http://172.16.0.7:3333' //pc fag
-    // baseURL: 'http://192.168.1.120:3333' //pc casa
-    baseURL: 'http://10.0.1.137:3333' //pc binar
+     baseURL: 'http://192.168.1.120:3333' //pc casa
+    //baseURL: 'http://10.0.1.137:3333' //pc binar
 })
 
 export default api;
