5405a5a0a3097eb33bb01d9914524c15dbb92333
Bonom0 <caioazbonomo@gmail.com>
2025-03-17 15:53:39 -0300

    Importado componente Background

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,14 @@
 import React, {useContext} from "react";
 import { View, Text, Button } from "react-native";
 import { AuthContext } from "../../contexts/auth";
+import { Background } from "./styles";
 
 export default function Home(){
     const { signOut, user } = useContext(AuthContext);
 
     return(
-        <View>
-            <Text>TELA HOME</Text>
-            <Text>Nome: {user.name} </Text>
-            <Button
-                title="Sair da conta"
-                onPress={ () => signOut() }
-            />
-        </View>
+        <Background>
+            
+        </Background>
     )
 }
