856d148f0112d1a5fba164c3abc65fdee0bb3005
Bonom0 <caioazbonomo@gmail.com>
2025-03-17 16:17:06 -0300

    Importado Header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,13 +2,14 @@ import React, {useContext} from "react";
 import { View, Text, Button } from "react-native";
 import { AuthContext } from "../../contexts/auth";
 import { Background } from "./styles";
+import Header from "../../components/Header";
 
 export default function Home(){
     const { signOut, user } = useContext(AuthContext);
 
     return(
         <Background>
-            
+            <Header title="Minhas movimentações" />
         </Background>
     )
 }
