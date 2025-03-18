3ce336c9923b9925ab8418c8e8ff6a0baf52c41c
Bonom0 <caioazbonomo@gmail.com>
2025-03-18 11:15:48 -0300

    Arquivos para criação da tela de cadastro de novos registros

---
+++ src/pages/New/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Background, Input, SubmitButton, SubmitText } from "./styles";
+import Header from '../../components/Header'
+import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from "react-native";
+
+export default function New(){
+    const [labelInput, setLabelInput] = useState('');
+    const [valueInput, setValueInput] = useState('');
+    const [type, setType] = useState('receita');
+    
+    return(
+        <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() }>
+            <Background>
+                <Header title="Registrando" />
+
+                <SafeAreaView style={{marginTop: 14, alignItems: "center"}}>
+                    <Input
+                        placeholder="Descrição do registro"
+                        value={labelInput}
+                        onChangeText={ (text) => setLabelInput(text) }
+                    />
+
+                    <Input
+                        placeholder="Valor desejado"
+                        keyboardType="numeric"
+                        value={valueInput}
+                        onChangeText={ (text) => setValueInput(text) }
+                    />
+
+                    <SubmitButton>
+                        <SubmitText>Registrar</SubmitText>
+                    </SubmitButton>
+                </SafeAreaView>
+            </Background>
+        </TouchableWithoutFeedback>
+    )
+}
