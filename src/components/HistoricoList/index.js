b18cd763bb035fd8d3ec87768b67ad0a26169a00
Bonom0 <caioazbonomo@gmail.com>
2025-03-18 14:20:41 -0300

    Criado estilos e layout dos cards de registros criados no dia

--- src/components/HistoricoList/index.js
+++ src/components/HistoricoList/index.js
@@ -1,10 +1,35 @@
 import React from "react";
-import { Container, TipoText } from "./styles";
+import { 
+    Container, 
+    TipoText,
+    Tipo,
+    IconView,
+    ValorText,
+    DescriptionText
+} from "./styles";
+import Icon from "react-native-vector-icons/Feather";
 
-export default function HistoricoList(){
+export default function HistoricoList({ data }){
     return(
         <Container>
-            <TipoText> Teste tipo </TipoText>
+            <Tipo>
+                <IconView tipo={data.type}>
+                    <Icon 
+                        name={data.type === 'despesa' ? 'arrow-down' : 'arrow-up'}
+                        size={20} 
+                        color="#FFF" 
+                    />
+                    <TipoText>{data.type}</TipoText>
+                </IconView>
+            </Tipo>
+
+            <ValorText>
+                R$ {data.value}
+            </ValorText>
+
+            <DescriptionText>
+                {data.description}
+            </DescriptionText>
         </Container>
     )
 }
