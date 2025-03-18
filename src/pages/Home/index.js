0e59dd672a5dbaeee9a9ec4005be28bb9db45d07
Bonom0 <caioazbonomo@gmail.com>
2025-03-18 10:59:48 -0300

    Importado componente flatlist dos cards de saldos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,18 @@
 import React, { useContext, useEffect, useState } from "react";
 import { View, Text, Button } from "react-native";
 import { AuthContext } from "../../contexts/auth";
-import { Background } from "./styles";
+import { 
+    Background, 
+    ListBalance 
+} from "./styles";
 import Header from "../../components/Header";
 import api from "../../services/api";
 import { format } from "date-fns";
+import { useIsFocused } from "@react-navigation/native";
+import BalanceItem from "../../components/BalanceItem";
 
 export default function Home(){
+    const isFocused = useIsFocused();
     const { signOut, user } = useContext(AuthContext);
     const [listBalance, setListBalance] = useState([]);
 
@@ -33,11 +39,19 @@ export default function Home(){
 
         return () => isActive = false;
 
-    }, [])
+    }, [isFocused])
 
     return(
         <Background>
             <Header title="Minhas movimentações" />
+
+            <ListBalance
+                data={listBalance}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={ item => item.tag }
+                renderItem={ ({ item }) => ( <BalanceItem data={item} /> ) }
+            />
         </Background>
     )
 }
