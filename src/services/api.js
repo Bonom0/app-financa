1e2b4b4e8ff802871d5cb7bb79ae5be342a084a0
Bonom0 <caioazbonomo@gmail.com>
2025-03-24 15:13:09 -0300

    Corrigido ip binar

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
     // baseURL: 'http://172.16.0.7:3333' //pc fag
     // baseURL: 'http://192.168.1.120:3333' //pc casa
-    baseURL: 'http://10.0.1.188:3333' //pc binar
+    baseURL: 'http://10.0.1.176:3333' //pc binar
 })
 
 export default api;
